2e1fe91e18b45ab7ffc6781e6c6805ea18cc620f
Ryan Harris <ryanh96@me.com>
2024-08-13 19:30:34 -0400

    Format

--- code/FPS/Weapons/Pistol.cs
+++ code/FPS/Weapons/Pistol.cs
@@ -1,62 +1,71 @@
 
 public class Pistol : Weapon
 {
-	[Property] public float ViewmodelXOffset { get; set; } = 30f;
-	[Property] public float ViewmodelYOffset { get; set; } = 9f;
-	[Property] public float ViewmodelZOffset { get; set; } = -10f;
+    [Property] public float ViewmodelXOffset { get; set; } = 30f;
+    [Property] public float ViewmodelYOffset { get; set; } = 9f;
+    [Property] public float ViewmodelZOffset { get; set; } = -10f;
     public int MaxReserves = 15;
     public int Reserves;
 
     public int MaxAmmo = 8;
     public int CurrentAmmo;
-	private SceneTraceResult lastTraceResult;
-    public Pistol(){
+    private SceneTraceResult lastTraceResult;
+    public Pistol()
+    {
         Log.Info("Instantiated Pistol");
         CurrentAmmo = MaxAmmo;
         Reserves = MaxReserves;
     }
-    
-	protected override void OnUpdate()
-	{
-		if ( !IsProxy )
-		{
+
+    protected override void OnUpdate()
+    {
+        if (!IsProxy)
+        {
             // Debug draw the last projectile fired.
             Gizmo.Draw.SolidSphere(lastTraceResult.EndPosition, 2.0f);
-		}
-	}
+        }
+    }
 
     public override void Fire()
     {
-        if (CurrentAmmo <= 0) 
+        if (CurrentAmmo <= 0)
         {
             Info("Empty!");
         }
-        else 
+        else
         {
             CurrentAmmo--;
             Info("Fired!");
 
-			float dist = 10000.0f;
-			lastTraceResult = Scene.Trace.Ray( new Ray(Transform.Position, Transform.Rotation.Forward * dist), dist ).Run();
+            float dist = 10000.0f;
+            // Shoot bullets directly out of the eyes (camera) of the parent (player)
+            var cam = GameObject.Parent.Components.GetInChildren<CameraComponent>();
+            Log.Info(cam);
+            if (cam != null)
+            {
+                Log.Info("xxxxxxxxxxx");
+                var origin = cam.Transform.Position;
+                lastTraceResult = Scene.Trace.Ray(new Ray(origin, Transform.Rotation.Forward * dist), dist).Run();
 
-			if ( lastTraceResult.Hit )
-			{
-				Log.Info( $"Hit: {lastTraceResult.GameObject} at {lastTraceResult.EndPosition}" );
-			}
+                if (lastTraceResult.Hit)
+                {
+                    Log.Info($"Hit: {lastTraceResult.GameObject} at {lastTraceResult.EndPosition}");
+                }
+            }
         }
     }
 
     // HL2 Style reloading. Pool of reserve bullets that refill a fixed magazine size. 
     public override void Reload()
     {
-        if (CurrentAmmo == MaxAmmo) 
+        if (CurrentAmmo == MaxAmmo)
         {
             Info("Already reloaded!");
         }
-        else if (Reserves <= 0) 
+        else if (Reserves <= 0)
         {
             Info("Out of reserves!");
-        } 
+        }
         else
         {
             var ammoDiff = MaxAmmo - CurrentAmmo;
@@ -66,7 +75,7 @@ public class Pistol : Weapon
             {
                 Reserves -= ammoDiff;
                 CurrentAmmo = MaxAmmo;
-            } 
+            }
             // Else, ammoDiff is greater than reserves, so load mag with whatever is left in reserves.
             else
             {

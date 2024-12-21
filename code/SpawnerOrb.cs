e951518669f858770ad7cfbf498501b92d04eb7b
crush3000 <crushthreethousand@gmail.com>
2024-12-20 21:05:13 -0500

    Fix weird collision bug on unit spawn

--- code/SpawnerOrb.cs
+++ code/SpawnerOrb.cs
@@ -4,6 +4,8 @@ public class SpawnerOrb : ControlOrb
 	[Property] List<UnitPortraitTuple> spawnableUnitList {  get; set; }
 
 	private int localCurretPhase = 0;
+	private Vector3 groundPosition;
+
 	protected override void OnStart()
 	{
 		base.OnStart();
@@ -13,11 +15,16 @@ public class SpawnerOrb : ControlOrb
 			return;
 		}
 		ThisOrbType = OrbType.Spawner;
-		foreach(var unitType in spawnableUnitList) 
+
+		var groundRay = Scene.Trace.Ray(new Ray(Transform.Position, Vector3.Down), 5000.0F);
+		var tr = groundRay.Run();
+		groundPosition = tr.EndPosition;
+
+		foreach (var unitType in spawnableUnitList) 
 		{
 			if(unitType.PhaseUnlocked <= GameState.Local.matchPhase)
 			{
-				buttons.Add(new ConstructUnitButton('.', unitType.UnitPortraitImage, unitType.UnitPrefab, Transform.Position, unitType.UnitResourceCost, unitType.UnitCapacityCost));
+				buttons.Add(new ConstructUnitButton('.', unitType.UnitPortraitImage, unitType.UnitPrefab, groundPosition, unitType.UnitResourceCost, unitType.UnitCapacityCost));
 			}
 		}
 	}
